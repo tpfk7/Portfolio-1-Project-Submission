f8fc43410f83bde0ceddb601d64aeeb72a195dfc
tpfk7 <thomas.kerins3@mail.dcu.ie>
2023-01-20 18:37:02 +0000

    Add style to form & image to index

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,9 +1,11 @@
-  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-    x.className += " responsive";
-    } else {
-    x.className = "topnav";
-    }
+/* jshint esversion: 11 */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleNavbar() {
+  let topNav = document.getElementById("myTopnav");
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
+  } else {
+    topNav.className = "topnav";
   }
+}
